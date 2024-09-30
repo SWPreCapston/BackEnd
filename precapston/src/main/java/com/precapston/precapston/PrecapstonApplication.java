b12326b8c98a40b7c101eaf0660db804e813c035
goeka <goeka1337@gmail.com>
2024-09-30 22:44:58 +0900

    Initial project commit

---
+++ precapston/src/main/java/com/precapston/precapston/PrecapstonApplication.java
@@ -0,0 +1,13 @@
+package com.precapston.precapston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecapstonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecapstonApplication.class, args);
+	}
+
+}
