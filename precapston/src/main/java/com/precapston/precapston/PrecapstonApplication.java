518400d290e441f8786fcb02e5391a63088d1f8a
seungjae708 <choesj365@gmail.com>
2024-10-01 00:49:31 +0900

    Test:test

--- precapston/src/main/java/com/precapston/precapston/PrecapstonApplication.java
+++ precapston/src/main/java/com/precapston/precapston/PrecapstonApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PrecapstonApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PrecapstonApplication.class, args);
 	}
