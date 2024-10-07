9b579bea4811e105ba9c41e142efa2fc65276ca9
seungjae708 <choesj365@gmail.com>
2024-10-07 13:08:17 +0900

    Feat: BCryptPasswordEncoder 암호화 메소드 추가

--- precapston/src/main/java/com/precapston/precapston/config/SecurityConfig.java
+++ precapston/src/main/java/com/precapston/precapston/config/SecurityConfig.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() { // 암호화 메소드
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -19,17 +25,17 @@ public class SecurityConfig {
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated()
-                );
+                ); // 인가 작업
 
 
         http
                 .formLogin((auth) -> auth.loginPage("/login")
                         .loginProcessingUrl("/loginProc")
                         .permitAll()
-                );
+                ); // 로그인 페이지에 대한 자동작업
 
         http
-                .csrf((auth) -> auth.disable());
+                .csrf((auth) -> auth.disable()); //csrf 설정 작업
 
 
         return http.build();
