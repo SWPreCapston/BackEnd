137966537bbf4dbbcc7d8c288ab749591b960fd9
seungjae708 <choesj365@gmail.com>
2024-10-07 15:21:57 +0900

    Feat: 세션 설정(소멸 시간, 중복 로그인, 고정 보호)

--- precapston/src/main/java/com/precapston/precapston/config/SecurityConfig.java
+++ precapston/src/main/java/com/precapston/precapston/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated()
-                ); // 인가 작업
+                ); // 경로에 대한 인가 작업
 
 
         http
@@ -34,8 +34,20 @@ public class SecurityConfig {
                         .permitAll()
                 ); // 로그인 페이지에 대한 자동작업
 
+
         http
-                .csrf((auth) -> auth.disable()); //csrf 설정 작업
+                .csrf((auth) -> auth.disable()); // csrf 설정 작업
+
+
+        http
+                .sessionManagement((auth) -> auth
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true)); // 다중 로그인 설정 -> 세션 통제
+
+
+        http
+                .sessionManagement((auth) -> auth
+                        .sessionFixation().changeSessionId()); // 세션 고정 보호
 
 
         return http.build();
