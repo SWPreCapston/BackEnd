066ac32cfd071c5ea46428ccbf331421755a564b
JunghunnKim <jhtree010410@gmail.com>
2024-10-29 13:21:21 +0900

    FEAT:TextService gpt모델 gpt-4로 변경

--- precapston/src/main/java/com/precapston/precapston/service/TextService.java
+++ precapston/src/main/java/com/precapston/precapston/service/TextService.java
@@ -46,7 +46,7 @@ public class TextService {
         private List<Message> messages;
 
         public OpenAIRequest(String prompt) {
-            this.model = "gpt-3.5-turbo";
+            this.model = "gpt-4";
             this.messages = new ArrayList<>();
             this.messages.add(new Message("user", prompt));
         }
