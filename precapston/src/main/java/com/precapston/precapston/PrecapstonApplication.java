d198dd98d7d9679afedad61f0ca61d3406e9a1ee
seungjae708 <choesj365@gmail.com>
2024-10-29 14:24:42 +0900

    FEAT: 이미지 관련 코드 삭제(머지 때문에)

--- precapston/src/main/java/com/precapston/precapston/PrecapstonApplication.java
+++ precapston/src/main/java/com/precapston/precapston/PrecapstonApplication.java
@@ -1,23 +1,8 @@
 package com.precapston.precapston;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 //import com.precapston.precapston.controller.ImageController;
-import com.precapston.precapston.dto.ImageDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 @SpringBootApplication
