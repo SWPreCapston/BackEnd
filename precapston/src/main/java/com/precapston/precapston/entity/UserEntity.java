016e46fe69daebfd2c569c0df943778e42bd4bac
seungjae708 <choesj365@gmail.com>
2024-11-05 00:20:39 +0900

    FEAT: 회원가입 시 name, phoneNumber 받도록 수정

--- precapston/src/main/java/com/precapston/precapston/entity/UserEntity.java
+++ precapston/src/main/java/com/precapston/precapston/entity/UserEntity.java
@@ -1,6 +1,7 @@
 package com.precapston.precapston.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,9 +14,21 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(unique = true) // 컬럽 값이 유니크
+    @Column(nullable = false, length = 20, unique = true)
+    @NotBlank(message = "Username cannot be empty")
     private String username;
+
+    @Column(nullable = false, length = 255)
+    @NotBlank(message = "Password cannot be empty")
     private String password;
+
+    @Column(nullable = false, length = 20)
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @Column(nullable = false, length = 20)
+    @NotBlank(message = "Phone number cannot be empty")
+    private String phoneNumber;
 
     private String role;
 }
